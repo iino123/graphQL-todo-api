7f30dd11431cf37e4020fe162ec313f4a89b79fb
iinokenjiro <zxcvbnm.ken@gmail.com>
2019-09-21 16:46:37 +0900

    fix: typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+    mount GraphiQL::Rails::Engine, at: '/graphql', graphql_path: '/graphql'
   end
 end
